90d90554549ad6104040449568f6d2417d2312f4
Henrik Ygge <henrik@meepo.se>
2021-04-04 17:37:47 +0200

    solved day 1

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Day1 {
+import util.Util;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        var input = Util.readString();
+        Util.submitPart1(part1(input));
+        Util.submitPart2(part2(input));
+    }
+
+    private static int part2(String input) {
+        int floor = 0;
+        for (int i = 0; i < input.length(); ++i) {
+            floor += input.charAt(i) == '(' ? 1 : -1;
+            if (floor == -1) {
+                return i+1;
+            }
+        }
+        throw new RuntimeException("Never got to floor -1");
+    }
+
+    private static int part1(String input) {
+        int floor = 0;
+        for (int i = 0; i < input.length(); ++i) {
+            floor += input.charAt(i) == '(' ? 1 : -1;
+        }
+        return floor;
+    }
 }
